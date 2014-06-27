38fef542264b817ebd66e31d8caf6431fb1ae728
yuuAn <nora@yuuan.net>
2014-06-27 19:04:51 +0900

    Mac用の設定を追加

--- zsh/dircolors-solarized.zsh
+++ zsh/dircolors-solarized.zsh
@@ -1,3 +1,12 @@
 # Load dircolors-solarized
-eval $(dircolors $ZSHHOME/dircolors-solarized/dircolors.ansi-universal)
+if which dircolors > /dev/null; then
+	eval $(dircolors $ZSHHOME/dircolors-solarized/dircolors.256dark)
+elif which gdircolors > /dev/null; then
+	eval $(gdircolors $ZSHHOME/dircolors-solarized/dircolors.256dark)
+fi
+
+# dircolors-solarized for zsh
+if [ -n "$LS_COLORS" ]; then
+	zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+fi
 
