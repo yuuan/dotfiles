b75d2eafcbe6ea6983c48c24bdb2cdd169369be7
yuuAn <nora@yuuan.net>
2014-06-27 22:53:40 +0900

    コマンド履歴の表示にpecoを使ってみる

---
+++ zsh/peco.zsh
@@ -0,0 +1,20 @@
+function peco-select-history() {
+	local tac
+	if which tac > /dev/null; then
+		tac="tac"
+	elif which gtac > /dev/null; then
+		tac="gtac"
+	else
+		tac="tail -r"
+	fi
+
+	BUFFER=$(history -n 1 | eval $tac | peco --query "$LBUFFER")
+	CURSOR=$#BUFFER
+	zle clear-screen
+}
+
+if which peco > /dev/null; then
+	zle -N peco-select-history
+	bindkey '^r' peco-select-history
+fi
+
