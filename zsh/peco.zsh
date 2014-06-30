9ff4f127eef994a438f1a276b93192a642bf9e3f
yuuAn <nora@yuuan.net>
2014-06-30 17:40:41 +0900

    コマンドが存在するか確認するときにエラーを出力しないように修正

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -1,8 +1,8 @@
 function peco-select-history() {
 	local tac
-	if which tac > /dev/null; then
+	if which tac &> /dev/null; then
 		tac="tac"
-	elif which gtac > /dev/null; then
+	elif which gtac &> /dev/null; then
 		tac="gtac"
 	else
 		tac="tail -r"
@@ -13,7 +13,7 @@ function peco-select-history() {
 	zle clear-screen
 }
 
-if which peco > /dev/null; then
+if which peco &> /dev/null; then
 	zle -N peco-select-history
 	bindkey '^r' peco-select-history
 fi
