f5457830f9ba80518de1d8d11dee685caa75e5f0
yuuAn <nora@yuuan.net>
2014-11-02 03:11:24 +0900

    ~/.vim/bundle/ があっても他のディレクトリは作成するように修正

--- install-vim.sh
+++ install-vim.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+mkdir -p $HOME/.vim/backup/
+mkdir -p $HOME/.vim/swp/
+mkdir -p $HOME/.vim/undo/
+
 if [ ! -d $HOME/.vim/bundle/ ]; then
-	mkdir -p $HOME/.vim/backup/
-	mkdir -p $HOME/.vim/swp/
-	mkdir -p $HOME/.vim/undo/
 	mkdir -p $HOME/.vim/bundle/
 	git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
 fi
