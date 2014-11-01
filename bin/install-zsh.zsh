8649acc373939aadb226d5b6c0675dbee6d7a07c
yuuAn <nora@yuuan.net>
2014-11-02 03:52:08 +0900

    .zshrc のインストールスクリプトを作成

---
+++ bin/install-zsh.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+function cyan-echo {
+	echo -e "\e[32m$*\e[m"
+}
+
+dotfiles="$(cd "$(dirname "$(dirname "${BASH_SOURCE:-${(%):-%N}}")")"; pwd)"
+
+cyan-echo "dotfiles directory is \"$dotfiles/\""
+
+cyan-echo "> ln -sni $dotfiles/zsh $HOME/.zsh"
+ln -sni $dotfiles/zsh $HOME/.zsh
+
+cyan-echo "> ln -si $dotfiles/zshrc $HOME/.zshrc"
+ln -si $dotfiles/zshrc $HOME/.zshrc
+
