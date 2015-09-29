c804caedddf0dd17e456fc4d2338df435b108a2d
yuuAn <nora@yuuan.net>
2015-09-29 13:27:02 +0900

    複数の SSH セッションを開いても pageant できるように

--- zsh/pageant.zsh
+++ zsh/pageant.zsh
@@ -1,18 +1,27 @@
 # pageant.exe
 
-LOCAL_TMP_DIR=$HOME/.ssh-agent
+if [ -n "$SSH_CLIENT" ]; then
+	LOCAL_TMP_DIR=$HOME/.ssh-agent
 
-mkdir -p $LOCAL_TMP_DIR
+	if [ ! -d $LOCAL_TMP_DIR ]; then
+		mkdir -p $LOCAL_TMP_DIR
+	else
+		find -L $LOCAL_TMP_DIR -type l | while read f; do
+			rm "$f"
+		done
+	fi
 
-agent="$LOCAL_TMP_DIR/ssh-agent-$USER"
-if [ -S "$SSH_AUTH_SOCK" ]; then
-	case $SSH_AUTH_SOCK in
-	/tmp/*/agent.[0-9]*)
-		ln -snf "$SSH_AUTH_SOCK" $agent && export SSH_AUTH_SOCK=$agent
-	esac
-elif [ -S $agent ]; then
-	export SSH_AUTH_SOCK=$agent
-#else
-#	 echo "no ssh-agent"
+	client=`echo $SSH_CLIENT | awk '{print sprintf("%s-%s",$1,$2)}' | sed -e 's/[^0-9]/-/g'`
+	agent="$LOCAL_TMP_DIR/ssh-agent-$USER-$client"
+
+	if [ -S "$SSH_AUTH_SOCK" ]; then
+		case $SSH_AUTH_SOCK in
+		/tmp/*/agent.[0-9]*)
+			ln -snf "$SSH_AUTH_SOCK" $agent && export SSH_AUTH_SOCK=$agent
+		esac
+	elif [ -S $agent ]; then
+		export SSH_AUTH_SOCK=$agent
+	#else
+	#	 echo "no ssh-agent"
+	fi
 fi
-
