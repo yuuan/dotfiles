73d98c5e56b0e82180a1ec39c58067f812b253a8
yuuAn <nora@yuuan.net>
2016-01-19 22:04:36 +0900

    PID を `peco` で選ぶためのグローバルエイリアスを設定

---
+++ zsh/zshrc.d/aliases-process.zsh
@@ -0,0 +1,15 @@
+# ----------------------------------------
+# `peco` でプロセスを選択し PID を取得
+# ----------------------------------------
+
+function process-id {
+	if [[ `whoami` = 'root' ]]; then
+		ps aux | peco --prompt "PROCESS>" | awk '{print $2}'
+	else
+		ps aux | grep "^$USER" | peco --prompt "PROCESS>" | awk '{print $2}'
+	fi
+}
+
+if which peco &> /dev/null; then
+	alias -g @PID='$(process-id)'
+fi
