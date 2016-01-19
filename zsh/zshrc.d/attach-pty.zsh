49d72c60e67c3d04fa5b21be0b84ad7fadafcf63
yuuAn <nora@yuuan.net>
2016-01-19 22:05:18 +0900

    コマンドが存在しなかった場合にエラーを表示

--- zsh/zshrc.d/attach-pty.zsh
+++ zsh/zshrc.d/attach-pty.zsh
@@ -3,10 +3,16 @@
 # ----------------------------------------
 
 function attach-pty {
-	if which reptyr &> /dev/null && which peco &> /dev/null; then
-		local process=$(ps aux | grep "^$USER" | peco | awk '{print $2}')
-		[[ -n "$process" ]] && reptyr "$process"
-	else
-		echo "reptyr is not installed."
+	if ! which reptyr &> /dev/null; then
+		echo -e "\e[31mreptyr is not installed.\e[m"
+		exit 1
 	fi
+
+	if ! which peco &> /dev/null; then
+		echo -e "\e[31mpeco is not installed.\e[m"
+		exit 1
+	fi
+
+	local process=$(ps aux | grep "^$USER" | peco | awk '{print $2}')
+	[[ -n "$process" ]] && reptyr "$process"
 }
