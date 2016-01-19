c3e65dfc01cf98ff5bb31fd338c95a846db2aa96
yuuAn <nora@yuuan.net>
2016-01-19 22:03:04 +0900

    Git に関するグローバルエイリアスを設定

---
+++ zsh/zshrc.d/aliases-git.zsh
@@ -0,0 +1,27 @@
+# ----------------------------------------
+# `git` コマンドに関する設定
+# ----------------------------------------
+
+function git-hash() {
+	git log --oneline --branches | peco --prompt "GIT COMMIT>" | awk '{print $1}'
+}
+
+function git-branch() {
+	git branch -a | peco --prompt "GIT BRANCH>" | head -n 1 | sed -e "s/^\*\s*//g"
+}
+
+function git-changed-files() {
+	git status --short | peco --prompt "CHANGES FILES" | awk '{print $2}'
+}
+
+if which peco &> /dev/null; then
+	alias -g @HASH='$(git-hash)'
+	alias -g @H='$(git-hash)'
+	alias -g @BRANCH='$(git-branch)'
+	alias -g @B='$(git-branch)'
+	alias -g @FILE='$(git-changed-files)'
+	alias -g @F='$(git-changed-files)'
+fi
+
+alias st='git status'
+alias gb='git branch -a'
