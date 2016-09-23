3d1d977e105c34b6a606a8d0a253aa774bb80263
yuuAn <nora@yuuan.net>
2016-09-23 22:40:00 +0900

    artisan コマンドの補完を強化

--- zsh/zshrc.d/completions.zsh
+++ zsh/zshrc.d/completions.zsh
@@ -38,3 +38,12 @@ zstyle ':completion:*' use-cache true
 
 # `sudo` の後に補完するコマンドのパス
 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
+
+# 補完関数の表示を強化する
+zstyle ':completion:*' verbose yes
+#zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
+zstyle ':completion:*:messages' format '%F{yellow}%d%f'
+zstyle ':completion:*:warnings' format '%F{red}No matches for: %F{yellow}%d%f'
+zstyle ':completion:*:descriptions' format '%F{yellow}- %d -%f'
+zstyle ':completion:*:corrections' format '%F{yellow}%d %F{red}(errors: %e)%f'
+zstyle ':completion:*:options' description 'yes'
