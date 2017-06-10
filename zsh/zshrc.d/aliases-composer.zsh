5a9b0817248f5a461e18acccfa12308239ff285e
yuuAn <nora@yuuan.net>
2017-06-10 16:38:02 +0900

    composer に Packagist.jp を設定するためのエイリアスを追加

---
+++ zsh/zshrc.d/aliases-composer.zsh
@@ -0,0 +1,6 @@
+# ----------------------------------------
+# Composer のためのエイリアスの設定
+# ----------------------------------------
+
+alias composer-set-packagist-jp='composer config -g repos.packagist composer https://packagist.jp'
+alias composer-unset-packagist-jp='composer config -g --unset repos.packagist'
