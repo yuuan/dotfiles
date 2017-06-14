cb9ea06c55ef14379dca9888f45bad40e020178d
yuuAn <nora@yuuan.net>
2017-06-14 18:39:52 +0900

    `php-cs-fixer` を簡単に呼び出す関数を作成

--- zsh/zshrc.d/aliases-php.zsh
+++ zsh/zshrc.d/aliases-php.zsh
@@ -2,4 +2,10 @@
 # Laravel のためのエイリアスの設定
 # ----------------------------------------
 
+function phpfix {
+	local dryrun options=${${@:#-f}:#--force}
+	[[ "${options}" = "${@}" ]] && dryrun="--dry-run"
+	php-cs-fixer fix --diff --ansi ${options:-} ${dryrun:-} | less -R -F
+}
+
 alias artisan="php artisan"
