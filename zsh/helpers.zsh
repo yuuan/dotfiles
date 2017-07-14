78c5aeafb83cebbf6745dc2fb0e6a46458fa8d85
yuuAn <nora@yuuan.net>
2017-07-14 15:02:47 +0900

    ヘルパー関数に接頭辞を付与

--- zsh/helpers.zsh
+++ zsh/helpers.zsh
@@ -1,5 +1,5 @@
 # 指定したファイルを読み込む
-__source() {
+__zshrc_source() {
 	local f
 	for f in "$@"; do
 		[ \( -f "$f" -o -h "$f" \) -a -r "$f" ] && source "$f"
@@ -7,22 +7,13 @@ __source() {
 }
 
 # 指定したディレクトリ内の `.zsh` ファイルを読み込む
-__sources() {
+__zshrc_sources() {
 	local d f
 	for d in "$@"; do
 		if [ -n "$d" -a -d "$d" -a -r "$d" -a -x "$d" ]; then
 			for f in $d/*; do
-				[[ ${f##*/} = *.zsh ]] && __source "$f"
+				[[ ${f##*/} = *.zsh ]] && __zshrc_source "$f"
 			done
 		fi
 	done
-}
-
-# 文字列をシェルコマンドの引数に与えても安全なようにエスケープする
-sh-escape() {
-	local s a=() q="'" qq='"'
-	for s in "$@"; do
-		a+=("'${s//$q/$q$qq$q$qq$q}'")
-	done
-	echo "${a[*]}"
 }
