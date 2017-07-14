d5f4de6bf93035c4ccbe8cd3c2fcd85803087092
yuuAn <nora@yuuan.net>
2017-07-14 20:47:41 +0900

    NeoVim があればそれを使う

--- zsh/zshrc.d/core/editor.zsh
+++ zsh/zshrc.d/core/editor.zsh
@@ -2,8 +2,10 @@
 # 環境変数 `EDITOR` の設定
 # ----------------------------------------
 
-if which vim &> /dev/null; then
-	export EDITOR=`which vim`
+if which nvim &> /dev/null; then
+	export EDITOR=nvim
+elif which vim &> /dev/null; then
+	export EDITOR=vim
 elif which vi &> /dev/null; then
-	export EDITOR=`which vi`
+	export EDITOR=vi
 fi
