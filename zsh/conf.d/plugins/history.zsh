3ed309eaf6f9d8661fc6561cc4eeae1a4d270dc8
yuuAn <nora@yuuan.net>
2022-07-04 14:05:22 +0900

    `history` コマンドのデフォルトのフィルタを無効化

--- zsh/conf.d/plugins/history.zsh
+++ zsh/conf.d/plugins/history.zsh
@@ -8,8 +8,8 @@ export ZSH_HISTORY_KEYBIND_GET="^r"
 # 履歴の履歴の同期を行わない
 export ZSH_HISTORY_AUTO_SYNC=false
 
-# 絞り込み条件を上書き
-export ZSH_HISTORY_FILTER_OPTIONS=
+# 絞り込み条件を上書き (値が無いとデフォルトが使われる)
+export ZSH_HISTORY_FILTER_OPTIONS=" "
 
 # `b4b4r07/history` のエイリアス
 alias history-b4b4r07="command history"
