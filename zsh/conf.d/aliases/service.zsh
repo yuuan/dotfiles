8359223a3f10baf319c111936d30f04b6253a1fe
yuuAn <nora@yuuan.net>
2025-02-25 14:14:44 +0900

    service コマンドはもう使ってないので削除

--- zsh/conf.d/aliases/service.zsh
+++
@@ -1,9 +0,0 @@
-# ----------------------------------------
-# `wheel` に属するユーザに対する設定
-# ----------------------------------------
-
-if [ `groups | tr " " "\n" | grep "^wheel$"` ]; then
-	if [ -f /sbin/service ]; then
-		alias service='sudo /sbin/service'
-	fi
-fi
