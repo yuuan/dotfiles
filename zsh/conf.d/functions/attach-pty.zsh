81d3d32281992f87ccf9a1dde3624edeb847f96c
yuuAn <nora@yuuan.net>
2025-02-25 14:15:14 +0900

    関数内で exit しちゃってたのを return に修正

--- zsh/conf.d/functions/attach-pty.zsh
+++ zsh/conf.d/functions/attach-pty.zsh
@@ -5,12 +5,12 @@
 function attach-pty {
 	if ! which reptyr &> /dev/null; then
 		echo -e "\e[31mreptyr is not installed.\e[m"
-		exit 1
+		return 1
 	fi
 
 	if ! which peco &> /dev/null; then
 		echo -e "\e[31mpeco is not installed.\e[m"
-		exit 1
+		return 1
 	fi
 
 	local process=$(ps aux | grep "^$USER" | peco | awk '{print $2}')
