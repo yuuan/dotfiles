0b43dabdccd249e2397140b9eb6dcc77aabe2307
yuuAn <nora@yuuan.net>
2025-03-01 18:37:02 +0900

    コマンドを実行しその記録を Atuin に追加する関数を作成

--- zsh/conf.d/services/history.zsh
+++ zsh/conf.d/services/history.zsh
@@ -16,6 +16,38 @@ function __services::history::is_installed {
 	(( $+commands[atuin] ))
 }
 
+# コマンドを実行しその履歴を Autin に保存
+function __services::history::eval {
+	local CMD=$1
+
+	if [[ -z $CMD ]]; then
+		return 127
+	fi
+
+	local ATUIN_HISTORY_ID EXIT DURATION END_TIME
+	local START_TIME=${EPOCHREALTIME-}
+
+	if __services::history::is_installed; then
+		ATUIN_HISTORY_ID=$(atuin history start -- "$CMD")
+		START_TIME=${EPOCHREALTIME-}
+	fi
+
+	eval "$CMD"
+
+	EXIT=$?
+
+	if __services::history::is_installed; then
+		END_TIME=${EPOCHREALTIME-}
+		if [[ -n $START_TIME && -n $END_TIME ]]; then
+			printf -v DURATION %.0f $(((END_TIME - START_TIME) * 1000000000))
+		fi
+
+		(ATUIN_LOG=error atuin history end --exit $EXIT ${DURATION:+--duration=$DURATION} -- $ATUIN_HISTORY_ID &) >/dev/null 2>&1
+	fi
+
+	return $EXIT
+}
+
 # Ctrl-R で履歴を表示
 if ! __services::history::is_installed; then
 	if (( $+commands[fzf-tmux] )); then
