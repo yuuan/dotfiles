6fc43403e4eedf610e65bff5d14b7de9c0ce0765
yuuAn <nora@yuuan.net>
2025-08-22 17:42:58 +0900

    PowerShell から wsl コマンドを実行したときにストア版の wsl が起動するようにする

--- pwsh/CurrentUserCurrentHost.ps1
+++ pwsh/CurrentUserCurrentHost.ps1
@@ -4,3 +4,7 @@
 # History
 # https://github.com/kelleyma49/PSFzf
 Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# use store version of WSL
+# https://scrapbox.io/c18t/Windows%E3%81%AEOpenSSH_Server%E3%81%AB%E6%8E%A5%E7%B6%9A%E3%81%97%E3%81%9F%E3%81%82%E3%81%A8%E3%80%81WSL%E3%82%92%E8%B5%B7%E5%8B%95%E3%81%A7%E3%81%8D%E3%81%AA%E3%81%84%E5%95%8F%E9%A1%8C
+Set-Alias wsl 'C:\Program Files\WSL\wsl.exe'
