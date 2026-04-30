2ac38d499c4d31130900605f85a4da070548fcef
yuuAn <nora@yuuan.net>
2026-04-30 15:56:34 +0900

    WezTerm の見た目の調整

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,33 +2,85 @@ local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local config = {}
 
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+-- Appearance / Window
+-- -------------------------------------------------------------
 
-config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
-config.window_padding = {
-  left = 8,
-  right = 8,
-  top = 10,
-  bottom = 0,
+local colors = {
+  active = {
+    background = { Color = "#b16286" },
+    foreground = { Color = "#fbf1c7" },
+  },
+  hover = {
+    background = { Color = "#665c54" },
+    foreground = { Color = "#fbf1c7" },
+  },
+  inactive = {
+    background = { Color = "#3c3836" },
+    foreground = { Color = "#fbf1c7" },
+  },
 }
+
+local characters = {
+  left_edge = wezterm.nerdfonts.ple_left_half_circle_thick,
+  right_edge = wezterm.nerdfonts.ple_right_half_circle_thick,
+}
+
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_background_opacity = 0.8
 config.window_frame = {
   font = wezterm.font('UDEV Gothic 35NF'),
   font_size = 14,
 }
+config.window_padding = {
+  left = 8,
+  right = 8,
+  top = 8,
+  bottom = 0,
+}
+
+config.use_fancy_tab_bar = true
 config.colors = {
   tab_bar = {
     inactive_tab_edge = "none",
   },
 }
+
+--config.hide_tab_bar_if_only_one_tab = true
+--config.show_new_tab_button_in_tab_bar = false
+--config.show_close_tab_button_in_tabs = false
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local status = tab.is_active and "active" or (hover and "hover" or "inactive")
+  local title = "  " ..
+      tab.tab_index + 1 .. ": " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "  "
+
+  return {
+    { Background = { Color = "none" } },
+    { Foreground = colors[status].background },
+    { Text = characters.left_edge },
+
+    { Background = colors[status].background },
+    { Foreground = colors[status].foreground },
+    { Text = title },
+
+    { Background = { Color = "none" } },
+    { Foreground = colors[status].background },
+    { Text = characters.right_edge },
+  }
+end)
+
+-- Appearance / Terminal
+-- -------------------------------------------------------------
+
 config.color_scheme = 'Japanesque'
 --config.color_scheme = 'GruvboxDark'
-config.window_background_opacity = 0.8
 config.font = wezterm.font 'HackGen Console NF'
 config.font_size = 17
 
 config.enable_scroll_bar = true
+
+-- Key Bindings
+-- -------------------------------------------------------------
 
 config.send_composed_key_when_left_alt_is_pressed = false
 config.keys = {
@@ -77,35 +129,6 @@ config.keys = {
 wezterm.on('gui-startup', function(cmd)
   local tab, pane, window = mux.spawn_window(cmd or {})
   window:gui_window():maximize()
-end)
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#626262"
-  local foreground = "#FFFFFF"
-
-  if tab.is_active then
-    background = "#ae8b2d"
-    foreground = "#FFFFFF"
-  end
-
-  local edge_background = "none"
-  local edge_foreground = background
-
-  local title = "  " .. tab.tab_index + 1 .. ": " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "  "
-
-  return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_LEFT_ARROW },
-
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = title },
-
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
-  }
 end)
 
 return config
